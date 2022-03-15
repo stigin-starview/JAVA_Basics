d1570e084790ff169621853268610bfad090008f
stigin-starview <stiginstarview@hotmail.com>
2022-03-15 12:11:36 +0530

    init

---
+++ SampleProjects/src/SampleFile.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class SampleFile {
+    public static void main(String ar[])
+    {
+        System.out.println("hello");
+        Scanner s= new Scanner(System.in);
+    }
+}
